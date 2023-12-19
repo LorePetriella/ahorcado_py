4390ce4152be0ada267c3dffcfef7f4eb2b8ae40
Lore Petriella <lorepetriella@gmail.com>
2023-12-18 22:02:51 -0300

    cambios al 18-12

--- app.py
+++ app.py
@@ -1,119 +1,8 @@
-
-# import random
-
-# lives = 10
-# chosen_letters = []
-# right_answers = []
-# wrong_answers = []
-# won_games = 0
-
-# def random_word():
-#     words = [
-#         'amarillo', 'azul', 'blanco', 'gris', 'casa', 'cielo', 'claro', 'color', 'comida', 'correr',
-#         'corto', 'creer', 'cruzar', 'cuadro', 'cuerpo', 'dama', 'dado', 'dulce', 'edad', 'elefante',
-#         'empezar', 'encontrar', 'entender', 'escalar', 'escribir', 'espacio', 'esperar', 'estrella', 'estudiar', 'feliz',
-#         'flor', 'frutilla', 'fruta', 'fuego', 'gato', 'girar', 'grande', 'gelatina', 'guitarra', 'hablar',
-#         'hacer', 'hermano', 'hijo', 'historia', 'hora', 'huevo', 'idea', 'igual', 'jugar', 'largo',
-#         'leer', 'libro', 'luz', 'madre', 'mano', 'mar', 'mesa', 'mirar', 'mover', 'mujer',
-#         'naranja', 'negro', 'nueve', 'nube', 'ojo', 'oso', 'padre', 'piso', 'palabra', 'papel',
-#         'parar', 'paso', 'paz', 'pozo', 'perro', 'pez', 'pie', 'piedra', 'pintar', 'plata',
-#         'poder', 'pollo', 'prueba', 'punto', 'querer', 'roto', 'risa', 'reloj', 'rojo', 'rosa',
-#         'saber', 'sal', 'saltar', 'sentir', 'ser', 'silla', 'sol', 'sonreir', 'suave', 'subir',
-#         'taza', 'termo', 'temprano', 'tiempo', 'tigre', 'tomar', 'toro', 'trabajar', 'tres', 'ver',
-#         'verde', 'viaje', 'volar', 'volver', 'zapato', 'zorro', 'abrir', 'aceite', 'agua', 'alto',
-#         'amigo', 'animal', 'dinosaurio', 'arena', 'bajo', 'boca', 'brazo', 'cabeza', 'calor', 'cama',
-#         'campo', 'carne', 'ciudad', 'clase', 'clave', 'cocina', 'comer', 'copa', 'carro', 'cuerda',
-#         'dormir', 'duro', 'escuela', 'familia', 'fiesta', 'flaco', 'fuerza', 'gafas', 'gente', 'gracias',
-#         'hambre', 'hombre', 'invierno', 'foto', 'joven', 'juego', 'lago', 'lento', 'libre', 'llave',
-#         'lluvia', 'luna', 'madera', 'manzana', 'mariposa', 'mes', 'mono', 'mesa', 'nadar', 'nieve',
-#         'nuevo', 'oro', 'pan', 'pelo', 'piedra', 'piel', 'pluma', 'puerta', 'queso', 'rata',
-#         'ropa', 'sopa', 'tigre', 'torta', 'vaca', 'viento', 'vino', 'vivir', 'yerba', 'zumo'
-#     ]
-#     return random.choice(words)
-
-
-# def view_random_word(randomWord):
-#    for index, letter in enumerate(randomWord):
-#       if index < len(randomWord) - 1:
-#           print("-", end=" ")
-#       else:
-#           print("-", end="")
-
-# def user_selection():
-#     return input("Por favor, elija una letra: ").lower()
-
-# def game(word, won_games, lives):
-#    result = ""
-    
-#    for item in word:
-   
-#          if item in chosen_letters:
-#             result += item
-                             
-#          else:
-#             result += '_'
-          
-
-    
-#    print(result)
-
-#    if result == word:
-#         won_games += 1
-        
-#         print("¡Adivinaste la palabra!", f"Partidas ganadas: {won_games}")        
-#         return result, won_games
-   
-#    user_guess = input("Si cree saber cuál es la palabra, escríbala a continuación o escriba 'no': ").lower()
-#    if user_guess == word:
-#         won_games += 1
-        
-#         print("¡Adivinaste la palabra!", f"Partidas ganadas: {won_games}")
-#         return word, won_games
-#    elif user_guess == 'no':
-#         return result, won_games
-#    else:
-#         print("Entrada no válida. Continuará el juego.")
-#         return result, won_games
-
-
-
-
-
-
-
-# chosen_word = random_word()
-# view_random_word(chosen_word)
-# print("\n" + chosen_word)
-
-# welcome = lambda lives: print(f"Bienvenido al juego! Tendrás {lives} chances para adivinar la palabra oculta")
-# welcome(lives)
-
-
-         
-
-# while lives >= 1:
-#     letter = user_selection()
-    
-#     if letter.isalpha():
-#         chosen_letters.append(letter)
-#         result = game(chosen_word, won_games, lives)
-
-#         if result == chosen_word:
-#             won_games += 1
-#             print("¡Avivinaste la palabra!", f"Partidas ganadas: {won_games}")
-#             break   
-        
-#     else:
-#         print("Por favor, ingrese una letra válida.")
-
-#     lives -= 1
-
-# print(f"Letras elegidas hasta ahora: {chosen_letters}")
-
 import random
 
 
 lives = 10
+wrong_letters = []
 
 
 def random_word():
@@ -140,6 +29,8 @@ def random_word():
     ]
     return random.choice(words)
 
+chosen_word = random_word()
+
 def view_random_word(randomWord):
     for index, letter in enumerate(randomWord):
         if index < len(randomWord) - 1:
@@ -156,11 +47,17 @@ def game(letter, word, chosen_letters, won_games, lives):
     for item in word:
         if item in chosen_letters:
             result += item
+            view_random_word(chosen_word)
         else:
             result += '_'
             lives -= 1
+            view_random_word(chosen_word)
+            # wrong_letters.append()
+            # print("\n" + f"La letra elegida no está en la palabra oculta. Las siguientes son las letras incorrectas hasta el momento: {}")
+
     
-    print(result)
+    print(result, sep = ' ')
+    
 
     if result == word:
         won_games += 1
@@ -168,6 +65,7 @@ def game(letter, word, chosen_letters, won_games, lives):
         return result, won_games
    
     user_guess = input("Si cree saber cuál es la palabra, escríbala a continuación o escriba 'no': ").lower()
+    print(result, sep = ' ')
     if user_guess == word:
         won_games += 1
         print("¡Adivinaste la palabra!", f"Partidas ganadas: {won_games}")
@@ -177,16 +75,19 @@ def game(letter, word, chosen_letters, won_games, lives):
     else:
         print("Entrada no válida. Continuará el juego.")
         return result, won_games
+    
+welcome = lambda lives: print(f"Bienvenido al juego! Tendrás {lives} chances para adivinar la palabra oculta")
 
 def play_game(lives):
     
     chosen_letters = []
     won_games = 0
-    chosen_word = random_word()
+    
+    print(chosen_word)
 
-    print("\n" + chosen_word)
+    welcome(lives)
 
-    welcome = lambda lives: print(f"Bienvenido al juego! Tendrás {lives} chances para adivinar la palabra oculta")
+   
 
     while lives >= 1:
         view_random_word(chosen_word)
